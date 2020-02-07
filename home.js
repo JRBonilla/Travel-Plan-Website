4b63e172c6f1a0db3c000491c1768e5cf20e219c
GodPlue <43499445+GodPlue@users.noreply.github.com>
2020-02-07 15:36:16 -0500

    Part 6
    Need rest of team info.

--- home.js
+++ home.js
@@ -123,6 +123,22 @@ function updateAttractions() {
   addOptions(attractionsSelect, options);
 }
 
+function aboutUs() {
+	  $("#aboutBox").show();
+}
+
+function contactUs() {
+	  $("#contactBox").show();
+}
+
+function closeContact() {
+	  $("#contactBox").hide();
+}
+
+function closeAbout() {
+	  $("#aboutBox").hide();
+}
+
 $( document ).ready(function() {
   $("#popularPlaces").on("change",function() {
     if (this.value == 'whiteHouse2') {
@@ -162,6 +178,8 @@ $( document ).ready(function() {
 
 $("#countries").hide();
 $("#attractions").hide();
+$("#contactBox").hide();
+$("#aboutBox").hide();
 addOptions(continentSelect, continents);
 addOptions(popularSelect, popularPlaces);
 
