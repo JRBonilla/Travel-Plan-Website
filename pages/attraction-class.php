67f26c7491b881c04614987649451c923c8fa835
Christopher Seow <christopherseow456@gmail.com>
2020-02-14 23:20:09 -0500

    iteration2 - initial
    -readMore now reads data from attraction class
    
    -attraction class read from db

---
+++ pages/attraction-class.php
@@ -0,0 +1,139 @@
+<?php
+
+class Attraction
+{  
+    private $attractId;
+    private $attractionName;
+    private $countryId;
+    private $typeId;    
+    private $dateOfCreation;
+    private $dimensions;
+    private $founder;
+    private $location;
+    private $imagePath1;
+    private $imagePath2;
+    private $imageAlt1;
+    private $imageAlt2;
+    private $user1;
+    private $userDate1;
+    private $comment1;
+    private $user2;
+    private $userDate2;
+    private $comment2;
+
+   // constructor
+   function __construct($attract_Id) 
+   { 
+       //connect to DB
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "cps630_assign1_db";
+        
+        //get attraction info based on passed id
+        $sqlQuery = "SELECT * FROM `tbl_attractions` WHERE `attract_id` = $attract_Id";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $result = $conn->query($sqlQuery);
+        $row = $result->fetch_assoc();
+        
+        $conn->close();
+
+        $this->attractId = $attract_Id;
+        $this->attractionName = $row["attraction_name"];
+        $this->countryId = $row["country_id"];
+        $this->typeId = $row["type_id"];
+        $this->dateOfCreation = $row["date-of-creation"];
+        $this->dimensions = $row["dimensions"];   
+        $this->founder = $row["founder"];
+        $this->location = $row["location"];
+
+        $this->imagePath1 = $row["image_path_1"];
+        $this->imagePath2 = $row["image_path_2"];
+        $this->imageAlt1 = $row["image_alt_1"];
+        $this->imageAlt2 = $row["image_alt_2"];
+        $this->user1 = $row["user_1"];
+        $this->userDate1 = $row["user_date_1"];   
+        $this->comment1 = $row["comment_1"];
+        $this->user2 = $row["user_2"];
+        $this->userDate2 = $row["user_date_2"];   
+        $this->comment2 = $row["comment_2"];
+   }    
+    
+   public function getAttractionName() 
+   {
+        return $this->attractionName;       
+   }
+   public function getCountryId() 
+   {
+        return $this->countryId;       
+   }
+   public function getTypeId() 
+   {
+        return $this->typeId;       
+   }
+   public function getDateOfCreation() 
+   {
+        return $this->dateOfCreation;       
+   }
+   public function getDimensions() 
+   {
+        return $this->dimensions;       
+   }
+   public function getFounder() 
+   {
+        return $this->founder;       
+   }
+   public function getLocation() 
+   {
+        return $this->location;       
+   }
+   public function getImagePath1() 
+   {
+        return $this->imagePath1;       
+   }
+   public function getImagePath2() 
+   {
+        return $this->imagePath2;       
+   }
+   public function getImageAlt1() 
+   {
+        return $this->imageAlt1;       
+   }
+   public function getImageAlt2() 
+   {
+        return $this->imageAlt2;       
+   }
+   public function getUser1() 
+   {
+        return $this->user1;       
+   }
+   public function getUserDate1() 
+   {
+        return $this->userDate1;       
+   }
+   public function getComment1() 
+   {
+        return $this->comment1;       
+   }
+   public function getUser2() 
+   {
+        return $this->user2;       
+   }
+   public function getUserDate2() 
+   {
+        return $this->userDate2;       
+   }
+   public function getComment2() 
+   {
+        return $this->comment2;       
+   }
+    
+}
+?>
---
+++ pages/readMorePages/readMore.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+    <!---By clicking on the “Read more”, a new page will appear which is divided into three parts:  
+        
+    -->
+    <head>
+        <link rel="stylesheet" type="text/css" href="../../assets/readMoreStyles.css">
+    </head>
+    <body>
+        <?php
+            session_start();
+            include('../attraction-class.php');
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "cps630_assign1_db";
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            $attract_type_id = "attract_type_id";
+            $this_attraction = new Attraction($_COOKIE[$attract_type_id]);
+            //$result = $conn->query($sqlQuery);
+            $conn->close();
+        ?>
+        <!--
+            - Part1 at the left and center that contains more small size images of the selected place (at least 2 more images), 
+        -->
+        <div id="part1">
+            <!--This is a container for the small size images for part 1-->
+            <h3>PICS</h3>
+            <img src=
+                <?php
+                    echo $this_attraction->getImagePath1();
+                ?> 
+            width='180' alt="">
+            <img src=
+                <?php
+                    echo $this_attraction->getImagePath2();
+                ?> 
+            width='180' alt="">
+        </div>
+        <!--
+            - Part2 at the right that contains more detailed description about the selected place e.g.: Date of creation, Name of the founder or builder, Dimensions, Location, Direction, and etc
+            The size of the small images should be fixed for any selected item. The shopping cart icon should appear on this page as well.     
+        -->
+        <div id="part2">
+            <h3>ABOUT THIS PLACE</h3>
+            <p>
+                <b>Date of Creation:</b>
+                <?php
+                    echo $this_attraction->getDateOfCreation();
+                ?>
+            </p>
+            <p>
+                <b>Dimensions:</b>
+                <?php
+                    echo $this_attraction->getDimensions();
+                ?>
+            </p>
+            <p>
+                <b>Founder:</b> 
+                <?php
+                    echo $this_attraction->getFounder();
+                ?>
+            </p>
+            <p>
+                <b>Location:</b> 
+                <?php
+                    echo $this_attraction->getLocation();
+                ?>
+            </p>
+        </div>
+        <!--        
+            - Part3 at the bottom contains “Reviews” or “Recent Posts”  about the selected place. Each post should appear with the name of the reviewer and the date and time that it was posted (at least 2 posts be seen)
+        -->
+        </br></br></br></br></br></br></br></br></br></br></br></br>
+        <div id="part3">
+            <h3>RECENT POSTS</h3>
+            <p>
+                <b>
+                    <?php
+                        echo $this_attraction->getUser1();
+                    ?>
+                </b>
+                <?php
+                    echo $this_attraction->getUserDate1();
+                ?>       
+            </p>
+            <p>
+                <?php
+                    echo $this_attraction->getComment1();
+                ?>   
+            </p>
+            </br>
+            <p>
+                <b>
+                    <?php
+                        echo $this_attraction->getUser2();
+                    ?>
+                </b>
+                <?php
+                    echo $this_attraction->getUserDate2();
+                ?>       
+            </p>
+            <p>
+                <?php
+                    echo $this_attraction->getComment2();
+                ?>   
+            </p>
+        </div>
+    </body>
+</html>
